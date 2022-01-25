85d70b973726c544163d1f4ce080fef7a87a261f
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-01-25 07:38:35 +0200

    finished part 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Keeper";
+
+function Header (){
+    return(<header><h1>{name}</h1></header>);
+}
+
+export default Header;
